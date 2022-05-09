aeb0eb5d2dd68c0adc4dd818a2962e52bd755fac
Linajja <94914937+linajja@users.noreply.github.com>
2022-05-09 22:13:49 +0300

    randcolor

--- src/Components/001/Briedis.jsx
+++ src/Components/001/Briedis.jsx
@@ -1,5 +1,13 @@
+import Random from "../../Funkcijos/rand";
+import randomColor from "../../Funkcijos/randomColor";
+
 function Briedis() {
-    return <h2>Aš briedis, o kas tu?</h2>
+    return <h2 style={
+        {
+            fontSize: Random(10,50)+'px',
+            color: randomColor()
+        }
+    }>Aš briedis, o kas tu?</h2>
 }
 
 export default Briedis;
--- src/Components/001/Kates.jsx
+++ src/Components/001/Kates.jsx
@@ -1,6 +1,12 @@
+import randomColor from "../../Funkcijos/randomColor";
+
 function Kates() {
     return (
-        <ul className="kates">
+        <ul className="kates" style={
+        {
+            color: randomColor()
+        }
+    }>
             <li>Pynkius</li>
             <li>Namy</li>
             <li>Pelėda</li>
